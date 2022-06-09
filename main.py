3ca46584c0913e6c1ff6a435de2a0fe422ffb22f
MariaJSanchezD <mariajsd00@gmail.com>
2022-06-09 16:40:25 +0200

    initial commit

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import smtplib
 
 from email.message import EmailMessage
@@ -8,15 +9,15 @@ from pathlib import Path
 email = EmailMessage()
 
 html = Template(Path('index.html').read_text())
-email['from'] = 'Maria Sanchez'
-email['to'] = 'y_loz@gmail.com'
+email['from'] = os.environ['from']
+email['to'] = os.environ['to']
 email['subject'] = 'Hello, this is an Email'
 
-email.set_content(html.substitute(name = 'Yushen'), 'html')
+email.set_content(html.substitute(name = os.environ['name']), 'html')
 
 with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
     smtp.ehlo()
     smtp.starttls()
-    smtp.login('pythonpracticexd@gmail.com', '123456Pp')
+    smtp.login(os.environ['email'], os.environ['password'])
     smtp.send_message(email)
     print('all good')
